847101848c765993028a7c5ddd2bb5fbadc10d07
lostboys08 <72627778+lostboys08@users.noreply.github.com>
2025-11-21 08:46:36 -0700

    Update cron.py

--- routes/cron.py
+++ routes/cron.py
@@ -80,7 +80,7 @@ async def daily_update(
         "job": "sync_bluestakes_tickets",
         "parameters": {
             "company_id": None,
-            "days_back": 2
+            "days_back": 14
         }
     }
 
